edeff700c951c0c2a93f5427a8cf23c38ced067e
poojithairosha <poojithairosha9311@gmail.com>
2024-07-05 11:38:35 +0530

    Improved authentication code

--- backend/src/main/java/com/poojithairosha/vristopos/controller/AuthController.java
+++ backend/src/main/java/com/poojithairosha/vristopos/controller/AuthController.java
@@ -5,33 +5,38 @@ import com.poojithairosha.vristopos.dto.AuthResponse;
 import com.poojithairosha.vristopos.dto.ClientResponse;
 import com.poojithairosha.vristopos.dto.ResetPasswordRequest;
 import com.poojithairosha.vristopos.service.AuthService;
+import com.poojithairosha.vristopos.util.UriProperties;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping(UriProperties.URI_AUTH)
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
+    @PostMapping(UriProperties.URI_LOGIN)
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest authRequest) {
+        log.info("Login request for user: {}", authRequest.username());
         return ResponseEntity.ok(authService.login(authRequest));
     }
 
-    @GetMapping("/forgot-password")
+    @GetMapping(UriProperties.URI_FP)
     public ResponseEntity<ClientResponse> forgotPassword(@NotNull @RequestParam String email) {
+        log.info("Forgot password request: {}", email);
         return ResponseEntity.ok(authService.forgotPassword(email));
     }
 
-    @PostMapping("/reset-password")
+    @PostMapping(UriProperties.URI_RP)
     public ResponseEntity<ClientResponse> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
+        log.info("Reset password request: {}", request);
         return ResponseEntity.ok(authService.resetPassword(request));
     }
-
 
 }
